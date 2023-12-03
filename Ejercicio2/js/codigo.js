9cb0a39cb474b5bf70042d748ff1524403a7bd66
Ainara <ainarabliz@gmail.com>
2023-12-03 10:57:18 +0100

    2 done

--- Ejercicio2/js/codigo.js
+++ Ejercicio2/js/codigo.js
@@ -1,6 +1,15 @@
-let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-meses.forEach(function(elemento, indice, array){
-    console.log(elemento, indice);
-});
+let qtty = prompt("Introduce la cantidad de numeros que quieres");
+let min = parseInt(prompt("Introduce el número mínimo"));
+let max = parseInt(prompt("Introduce el número máximo"));
+let num = [];
+
+aleatorio(qtty, min, max);
+console.log(num);
+
+function aleatorio(qtty, min, max) {
+    for (let i = 1; i <= qtty; i++) {
+        num.push(parseInt(Math.random() * (max - min) + min));
+    }
+}
 
 
